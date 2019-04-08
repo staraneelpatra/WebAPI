000636ab4bee4872ed838a71ba73065fb2ede98e
staraneel <33164249+staraneelpatra@users.noreply.github.com>
2019-04-08 22:01:23 +0530

    Check in

---
+++ WebAPIDemo/WebAPIDemo/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Web.Http;
+
+namespace WebAPIDemo.Controllers
+{
+    public class HomeController:ApiController
+    {
+        public string Get()
+        {
+            return "Hello Called From Get()";
+        }
+
+        public string get(string id)
+        {
+            if (id=="Musi"||id=="Smile"||id=="Snigdha")
+            {
+                return "Love You " + id;
+            }
+            return "Hatasya Premika";
+        }
+       
+    }
+}
