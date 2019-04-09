3c27d4f497ccf146ce2a8afc86e5ccc5843ae51c
staraneel <33164249+staraneelpatra@users.noreply.github.com>
2019-04-09 23:23:15 +0530

    checjked

--- WebAPIDemo/WebAPIDemo/Controllers/HomeController.cs
+++ WebAPIDemo/WebAPIDemo/Controllers/HomeController.cs
@@ -1,23 +1,33 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Web.Http;
 
 namespace WebAPIDemo.Controllers
 {
     public class HomeController:ApiController
     {
-        public string Get()
+        public static List<string> values = new List<string>() { "Anil", "Musi", "Dhani" };
+
+        public IEnumerable<string> Get()
         {
-            return "Hello Called From Get()";
+            return values;
         }
 
-        public string get(string id)
+        public string Get(int id)
         {
-            if (id=="Musi"||id=="Smile"||id=="Snigdha")
-            {
-                return "Love You " + id;
-            }
-            return "Hatasya Premika";
+            return values[id];
         }
-       
+       public void Post([FromBody]string newvalue)
+        {
+            values.Add(newvalue);
+        }
+        public void Put(int id, [FromBody]string newvalue)
+        {
+            values[id] = newvalue;
+        }
+        public void Delete(int id)
+        {
+            values.RemoveAt(id);
+        }
     }
 }
