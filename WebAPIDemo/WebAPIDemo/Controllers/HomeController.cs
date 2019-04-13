a56bd43ac949bb9205f902653b459ba9f88e1c49
staraneel <33164249+staraneelpatra@users.noreply.github.com>
2019-04-13 23:35:46 +0530

    commit

---
+++ WebAPIDemo/WebAPIDemo/Controllers/HomeController.cs
@@ -0,0 +1,7 @@
+﻿using System; using System.Collections.Generic; using System.Web.Http;  namespace WebAPIDemo.Controllers {     public class HomeController : ApiController     {         public static List<string> values = new List<string>() { "Anil", "Musi", "Dhani" };          public IEnumerable<string> Get()         {             return values;         }          public string Get(int id)         {             return values[id];         }         public void Post([FromBody]string newvalue)         {             values.Add(newvalue);         }         public void Put(int id, [FromBody]string newvalue)         {             values[id] = newvalue;         }         public void Delete(int id)         {
+            //
+            //commit
+            values.RemoveAt(id);         }         public void Delete1(int id)         {
+            //
+            //commit
+            values.RemoveAt(id);         }     } }   
