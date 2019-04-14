487b76131d2e603388b643337b39ba22bae95816
Snigdha Patra <snigdha.patra@hotmail.com>
2019-04-14 12:07:24 +0530

    commit

---
+++ WebAPISQL/WebAPISQL/Controllers/DefaultController.cs
@@ -0,0 +1,46 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using EmployeeDataAdapter;
+using System.Data;
+
+namespace WebAPISQL.Controllers
+{
+    public class DefaultController : ApiController
+    {
+        public IEnumerable<Employee> Get()
+        {
+            using (TharEntitiesConnection con = new TharEntitiesConnection())
+            {
+                return con.Employees.ToList();
+            }
+        }
+        public Employee Get(int id)
+        {
+            using (TharEntitiesConnection con = new TharEntitiesConnection())
+            {
+
+                return con.Employees.Where(x => x.Id == id).FirstOrDefault();
+                //return con.Employees.FirstOrDefault(x => x.Id == id);
+            }
+        }
+        public bool Put([FromUri]Employee value, int id )
+        {
+            using (TharEntitiesConnection con = new TharEntitiesConnection())
+            {
+
+                Employee emp = new Employee();
+                con.Employees.Where(x => x.Id == id);
+                {
+                    emp.Name = value.ToString();
+                }
+                return true;
+                
+            }
+        }
+    }
+}
---
+++ WebAPISQL/WebAPISQL/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebAPISQL.WebApiApplication" Language="C#" %>
